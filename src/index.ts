31e2ed9ae17346c771dab800a2bda8536cb2b713
handikatriarlan <handikaarlan@gmail.com>
2025-02-03 22:25:24 +0700

    feat(api): add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import { Routes } from "./routes"
 
 const app = new Hono().basePath("/api")
+
+app.use("*", cors())
 
 app.route("/todos", Routes)
 
