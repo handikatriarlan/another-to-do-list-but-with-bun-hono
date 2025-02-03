359ce54e07390bf37839ffaa5276d11bafefbb6a
handikatriarlan <handikaarlan@gmail.com>
2025-02-03 09:27:44 +0700

    feat(todo): add get todo by id endpoint

--- src/controllers/TodoController.ts
+++ src/controllers/TodoController.ts
@@ -62,3 +62,44 @@ export async function createTodo(c: Context) {
     )
   }
 }
+
+export async function getTodoById(c: Context) {
+  try {
+    const todoId = parseInt(c.req.param("id"))
+
+    const todo = await prisma.todo.findUnique({
+      where: { id: todoId },
+    })
+
+    if (!todo) {
+      //return JSON
+      return c.json(
+        {
+          success: false,
+          message: "Todo Not Found!",
+          data: null,
+        },
+        404
+      )
+    }
+
+    return c.json(
+      {
+        success: true,
+        message: `Detail Data Todo By ID : ${todoId}`,
+        data: todo,
+      },
+      200
+    )
+  } catch (e: unknown) {
+    console.error(`Error finding todo: ${e}`)
+    return c.json(
+      {
+        success: false,
+        message: `Error finding todo: ${e}`,
+        data: null,
+      },
+      500
+    )
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono"
-import { getTodos, createTodo } from "../controllers/TodoController"
+import { getTodos, createTodo, getTodoById } from "../controllers/TodoController"
 
 const router = new Hono()
 
 router.get("/", (c) => getTodos(c))
 router.post("/", (c) => createTodo(c))
+router.get("/:id", (c) => getTodoById(c))
 
 export const Routes = router
