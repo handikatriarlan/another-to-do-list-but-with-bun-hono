759bbe7255613043574c3329fe11e76020d44576
handikatriarlan <handikaarlan@gmail.com>
2025-02-03 09:50:49 +0700

    feat(todo): add update todo status endpoint

--- src/controllers/TodoController.ts
+++ src/controllers/TodoController.ts
@@ -162,11 +162,66 @@ export async function updateTodoTitle(c: Context) {
       200
     )
   } catch (e: unknown) {
-    console.error(`Error updating todo: ${e}`)
+    console.error(`Error updating todo title: ${e}`)
     return c.json(
       {
         success: false,
-        message: "An unexpected error occurred while updating todo.",
+        message: `Error updating todo title: ${e}`,
+        data: null,
+      },
+      500
+    )
+  }
+}
+
+export async function updateTodoStatus(c: Context) {
+  try {
+    const todoId = parseInt(c.req.param("id"))
+
+    if (isNaN(todoId)) {
+      return c.json(
+        { success: false, message: "Invalid ID format!", data: null },
+        400
+      )
+    }
+
+    const todo = await prisma.todo.findUnique({
+      where: { id: todoId },
+    })
+
+    if (!todo) {
+      return c.json(
+        {
+          success: false,
+          message: "Todo Not Found!",
+          data: null,
+        },
+        404
+      )
+    }
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: todoId },
+      data: {
+        completed: !todo.completed,
+        updatedAt: new Date(),
+      },
+    })
+
+    return c.json(
+      {
+        success: true,
+        message: "Todo Status Updated Successfully!",
+        data: updatedTodo,
+      },
+      200
+    )
+  } catch (e: unknown) {
+    console.error(`Error updating todo status: ${e}`)
+    return c.json(
+      {
+        success: false,
+        message: `Error updating todo status: ${e}`,
         data: null,
       },
       500
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import {
   createTodo,
   getTodoById,
   updateTodoTitle,
+  updateTodoStatus,
 } from "../controllers/TodoController"
 
 const router = new Hono()
@@ -11,6 +12,7 @@ const router = new Hono()
 router.get("/", (c) => getTodos(c))
 router.post("/", (c) => createTodo(c))
 router.get("/:id", (c) => getTodoById(c))
-router.patch("/:id", (c) => updateTodoTitle(c))
+router.patch("/:id/title", (c) => updateTodoTitle(c))
+router.patch("/:id/status", (c) => updateTodoStatus(c))
 
 export const Routes = router
