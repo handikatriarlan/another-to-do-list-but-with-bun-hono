65cdc32c98f329279b65d116514f880dec9140fb
handikatriarlan <handikaarlan@gmail.com>
2025-02-04 09:20:07 +0700

    feat(analytics): add vercel analytics package

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Hono } from "hono"
 import { cors } from "hono/cors"
 import { Routes } from "./routes"
+import { inject } from "@vercel/analytics"
 
-const app = new Hono().basePath("/api")
+const app = new Hono()
 
 app.use("*", cors())
 
-app.route("/todos", Routes)
+app.use("*", async (c, next) => {
+  inject()
+  await next()
+})
+
+app.route("/api/todos", Routes)
 
 export default app
